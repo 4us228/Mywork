6265706737659d9ef3d8cc82c2357972f5c4bdcf
chusi <4us.in.ua@gmail.com>
2022-04-21 09:55:59 +0300

    Add project files.

---
+++ Hw1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hw1
+{
+    internal class Anket
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
